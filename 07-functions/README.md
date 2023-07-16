a941fb36f23b30015c67c2c59c94d85ff45f7f94
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-07-16 13:21:51 -0300

    [functions] F: Adding Tutorial

---
+++ 07-functions/README.md
@@ -0,0 +1,4 @@
+# Functions
+
+This tutorial introduces you to the following concepts in Rust:
+- Functions
---
+++ 07-functions/src/main.rs
@@ -0,0 +1,55 @@
+// Copyright(C) 2023 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+// Licensed under the MIT license.
+
+///
+/// This tutorial introduces you to the following concepts in Rust:
+///  - Functions
+///
+fn main() {
+    let a: isize = -23;
+    let b: isize = 7;
+
+    print(a);
+    print(b);
+
+    let result: isize = add(a, b);
+    println!("{} + {} = {}", a, b, result);
+
+    let result: usize = abs(a);
+    println!("|{}| = {}", a, result);
+
+    /// This a nested function.
+    fn greater_than(x: isize, y: isize) -> bool {
+        x > y
+    }
+    print!("{} > {} ? ({})", a, b, greater_than(a, b));
+
+    // TODO: Activity 1
+    //  - Write a function that computes the factorial of a number.
+
+    // TODO: Activity 2
+    // - Write a function that computes the nth term of the Fibonacci sequence.
+}
+
+/// This is a function takes a value and does not return anything.
+/// The return type could be omitted.
+pub fn print(val: isize) -> () {
+    println!("val: {}", val);
+}
+
+//// This function takes two values as parameter, and returns a value.
+/// The return value is the last expression listed in the function.
+pub fn add(a: isize, b: isize) -> isize {
+    a + b
+}
+
+/// This function takes one value as parameter and returns a value.
+/// The `return` keyword may be omitted only for the last expression
+/// listed in t the function.
+pub fn abs(x: isize) -> usize {
+    if x < 0 {
+        return -x as usize;
+    }
+
+    x as usize
+}
